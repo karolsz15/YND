bf3f0c4818f610d4b90a79f8edc343412c31b7fc
Karol Szpek <karolsz15@gmail.com>
2020-07-28 13:55:38 +0200

    created basic HTML markup and CSS styling

---
+++ src/Container/Container.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+// import Dropdown from '../Components/Dropdown';
+import classes from './Container.module.css';
+import './Dropdown.css';
+
+class Container extends Component {
+    render () {
+        return (
+            <div className={classes.Container}>
+                <input type="text" className={classes.SearchInput} placeholder="Enter username"></input>
+                <button className={classes.SearchButton}>Search</button>
+
+                <input className={classes.DropdownContainer} id="toggle" type="checkbox" /><label for="toggle">Exampleuser 1</label>
+                <div id="wrap">
+                    <div id="slider">
+                        <div className={classes.SingleRepo}>
+                            <div className={classes.TitleAndDescription}>
+                                <div className={classes.RepoTitle}>
+                                    Repository title
+                                </div>
+                                <div className={classes.RepoDescription}>
+                                    Repository description
+                                </div>
+                            </div>
+                            <div>
+                                12 <i class="fa fa-star"></i>
+                            </div>  
+                        </div>
+
+                        <div className={classes.SingleRepo}>
+                            <div className={classes.TitleAndDescription}>
+                                <div className={classes.RepoTitle}>
+                                    Repository title
+                                </div>
+                                <div className={classes.RepoDescription}>
+                                    Repository description
+                                </div>
+                            </div>
+                            <div>
+                                48 <i class="fa fa-star"></i>
+                            </div>
+                        </div>
+
+                        <div className={classes.SingleRepo}>
+                            <div className={classes.TitleAndDescription}>
+                                    <div className={classes.RepoTitle}>
+                                        Repository title
+                                    </div>
+                                    <div className={classes.RepoDescription}>
+                                        Repository description
+                                    </div>
+                                </div>
+                            <div>
+                                32 <i class="fa fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        )
+    };
+};
+
+export default Container;
