3c609b6a5922108d93842830e74f637441da6986
Karol Szpek <karolsz15@gmail.com>
2020-07-29 18:53:02 +0200

    added dynamic display for 5 first users repositories

--- src/Components/SingleUser.js
+++ src/Components/SingleUser.js
@@ -31,71 +31,33 @@ const SingleUser = props => {
         });
     }
 
-    // useEffect( () => {
-    //     axios.get(`https://api.github.com/users/${props.username}/repos`)
-    //     .then( response => {
-    //         // handle success
-    //         setActiveUser(props.username);
-    //         setActiveReposDetails(response.data); //response.data is an array of objects
-    //         console.log(response.data);
-    //     })
-    //     .catch( error => {
-    //         // handle error
-    //         console.log(error);
-    //         setError();
-    //     });
-    // }, [ setActiveUser, setActiveReposDetails, setError]);
-    let listOfRepos = null;
+    let listOfRepos = [];
 
     if (props.activeReposDetails) {
-        singleRepoArray = _.values(props.activeReposDetails[0]); //making array out of object with lodash
-        singleRepoTitle = singleRepoArray[2];
-        singleRepoDescription = singleRepoArray[7];
-        singleRepoStars = singleRepoArray[71];
-        listOfRepos = (
-            <SingleRepo 
-                singleRepoTitle={singleRepoTitle} 
-                singleRepoDescription={singleRepoDescription} 
-                singleRepoStars={singleRepoStars} />
-        );
-    }
-
-    // listOfRepos = props.showListOfRepos ? (
-    //     <SingleRepo 
-    //         singleRepoTitle={singleRepoTitle} 
-    //         singleRepoDescription={singleRepoDescription} 
-    //         singleRepoStars={singleRepoStars} />
-    // ) : null;
-
-    // useEffect(() => {
-    //     listOfRepos = showListOfRepos ? (
-    //         <SingleRepo 
-    //             singleRepoTitle={singleRepoTitle} 
-    //             singleRepoDescription={singleRepoDescription} 
-    //             singleRepoStars={singleRepoStars} />
-    //     ) : null;
-    // }, [showListOfRepos])
-
+        for (let i = 0; i <5; i++) {
+            singleRepoArray = _.values(props.activeReposDetails[i]); //making array out of object with lodash
+            singleRepoTitle = singleRepoArray[2];
+            singleRepoDescription = singleRepoArray[7];
+            singleRepoStars = singleRepoArray[71];
+            listOfRepos.push(
+                <SingleRepo 
+                    singleRepoTitle={singleRepoTitle} 
+                    singleRepoDescription={singleRepoDescription} 
+                    singleRepoStars={singleRepoStars} />
+            );
+        };
+    };
 
     return (
         <React.Fragment>
-            {/* <input 
-                className={classes.DropdownContainer} 
-                id="toggle" 
-                type="checkbox"></input>
-            <label onClick={() => getUsersRepos()} for="toggle">{props.username}<i class="fa fa-angle-down" style={{fontSize:'1.5em'}}></i></label>
-                <div id="wrap">
-                    <div id="slider"> */}
+
                     <div onClick={() => getUsersRepos()} className={classes.DropdownContainer}>
                         {props.username}<i class="fa fa-angle-down" style={{fontSize:'1.5em', margin:'1em'}}></i>
                     </div>
+
                     {props.showListOfRepos ? listOfRepos : null}
                     
-                    {/* {showListOfRepos ? ( */}
-                        
-                    {/* ) : null} */}
-                    {/* </div>
-                </div>  */}
+ 
         </React.Fragment>
     );
 };
